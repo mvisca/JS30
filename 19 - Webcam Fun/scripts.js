8759a6e8e376bb1de2a0ea5f1820c627f17f7713
martin visca <martin.visca@gmail.com>
2021-08-02 15:46:32 +0200

    Filters

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -26,7 +26,11 @@ function paintToCanvas() {
     ctx.drawImage(video, 0, 0, width, height);
 
     let pixels = ctx.getImageData(0, 0, width, height);
-    pixels = effects(pixels, 'rgb');
+    ctx.globalAlpha = 1;
+
+    // The effect ==>>
+
+    pixels = effects(pixels, 'red');
 
     ctx.putImageData(pixels, 0, 0);
   }, 16);
@@ -58,14 +62,52 @@ function effects(pixels, effect = '') {
 
   if (effect === 'rgb') {
     for (let i = 0; i < pixels.data.length; i += 4) {
+      pixels.data[i - 250] = pixels.data[i];
+      pixels.data[i + 400] = pixels.data[i + 1];
+      pixels.data[i + 500] = pixels.data[i + 2];
+    }
+    return pixels;
+  }
+  return pixels;
+
+  if (effect === 'rgb') {
+    for (let i = 0; i < pixels.data.length; i += 4) {
       pixels.data[i - 150] = pixels.data[i];
       pixels.data[i + 300] = pixels.data[i + 1];
       pixels.data[i + 50] = pixels.data[i + 2];
     }
     return pixels;
   }
+
+  // if (effect === 'green') {
+
+  //   const levels = {};
+
+  //   document.querySelectorAll('.rgb input').forEach((input) => {
+  //     levels[input.name] = input.value;
+  //   });
+
+  //   for (i = 0; i < pixels.data.length; i = i + 4) {
+  //     red = pixels.data[i + 0];
+  //     green = pixels.data[i + 1];
+  //     blue = pixels.data[i + 2];
+  //     alpha = pixels.data[i + 3];
+
+  //     if (red >= levels.rmin
+  //       && green >= levels.gmin
+  //       && blue >= levels.bmin
+  //       && red <= levels.rmax
+  //       && green <= levels.gmax
+  //       && blue <= levels.bmax) {
+  //       // take it out!
+  //       pixels.data[i + 3] = 0;
+  //     }
+  //   }
+  //   return pixels;
+  // }
   return pixels;
 }
+
 
 getVideo();
 
